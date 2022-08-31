6ef3f3368af3151181eaef4ef644c2a059b33e3a
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-31 17:53:45 -0300

    Class SavingAccount, upcasting and downcasting tested

---
+++ Accounts/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace Accounts.Entities {
+    public class SavingsAccount : Account {
+        // Attributes
+        public double InterestRate { get; set; }
+
+        // Constructors
+        public SavingsAccount() {}
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) 
+            : base(number, holder, balance) {
+            InterestRate = InterestRate;
+        }
+
+        // Methods
+        public void UpdateBalance() {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
