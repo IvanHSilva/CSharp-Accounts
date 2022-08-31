6075d0004ca28de7d36cc4b09070a9fc8c303fb0
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-31 11:41:08 -0300

    Classes base and heritaged created

---
+++ Accounts/Entities/Account.cs
@@ -0,0 +1,26 @@
+﻿namespace Accounts.Entities {
+    public class Account {
+        // Attributes
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        // Constructors
+        public Account() { }
+
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        // Methods
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+    }
+}
---
+++ Accounts/Entities/BusinessAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace Accounts.Entities {
+    public class BusinessAccount : Account {
+        // Attributes
+        public double LoanLimit { get; set; }
+
+        // Constructor
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance) {
+            LoanLimit = loanLimit;
+        }
+
+        // Methods
+        public void Loan(double amount) {
+            if (amount <= LoanLimit) Balance += amount;
+        }
+    }
+}
